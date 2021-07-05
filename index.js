874964c36458913e2088d6e6809487014e9b5135
breannams <breannamarie_s@hotmail.com>
2021-07-05 16:30:30 -0400

    commit tree 577edb6110e424d3c89d76fcb164866e855e8cc4

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let max = Math.max(1,20)
+
+let random = Math.floor(Math.random() *10) +1
+
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2
+
+
+let num3 = 14  
+let num4 = 5
+
+let mod = num3 % num4
